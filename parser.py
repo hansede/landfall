7a3be68cfed9dc1851f8ea8d05a50cfac8094083
David Hansen <david@tourian.(none)>
2013-02-18 00:03:03 -0700

    Back to embedded

--- parser.py
+++ parser.py
@@ -9,18 +9,8 @@ import datetime
 from pymongo import Connection
 connection = Connection()
 landfall = connection.landfall
-landfall.drop_collection('releases')
-landfall.drop_collection('cards')
-landfall.drop_collection('values')
-landfall.drop_collection('cardtypes')
-landfall.drop_collection('colors')
-landfall.drop_collection('rarities')
+#landfall.drop_collection('cards')
 cards = landfall.cards
-values = landfall.values
-releases = landfall.releases
-types = landfall.types
-colors = landfall.colors
-rarities = landfall.rarities
 
 def getCardId(cardObj):
   card = cards.find_one(cardObj)
@@ -32,19 +22,7 @@ def getCardId(cardObj):
     
   return cardId
 
-def getReleaseId(name):
-  releaseObj = {'name': name}
-
-  release = releases.find_one(releaseObj)
-  
-  if release is None:
-    releaseId = releases.insert(releaseObj)
-  else:
-    releaseId = release["_id"]
-    
-  return releaseId
-
-def getCardTypeId(name):
+def fixCardType(name):
   if "enchantment" in name:
     name = "enchantment"
   elif "nstant" in name:
@@ -59,41 +37,20 @@ def getCardTypeId(name):
     name = "creature"
   elif "summon" in name:
     name = "creature"
+  return name
 
-  cardTypeObj = {'name': name}
-  myType = types.find_one(cardTypeObj)
-  
-  if myType is None:
-    typeId = types.insert(cardTypeObj)
-  else:
-    typeId = myType["_id"]
-    
-  return typeId
-
-def getColorId(name):
-  if name is "whtie":
+def fixColor(name):
+  if name == "whtie":
     name = "white"
-
-  colorObj = {'name': name}
-  color = colors.find_one(colorObj)
-  
-  if color is None:
-    colorId = colors.insert(colorObj)
-  else:
-    colorId = color["_id"]
-    
-  return colorId
-  
-def getRarityId(name):
-  rarityObj = {'name': name}
-  rarity = rarities.find_one(rarityObj)
-  
-  if rarity is None:
-    rarityId = rarities.insert(rarityObj)
-  else:
-    rarityId = rarity["_id"]
-    
-  return rarityId
+  elif name == "artifact":
+    name = "colorless"
+  elif name == "land":
+    name = "colorless"
+  elif name == "":
+    name = "colorless"
+  elif name == "gold":
+    name = "multi"
+  return name
     
 def processHtml(html):
   if html is not None:
@@ -108,63 +65,38 @@ def parseFloat(text):
 
 def parseSet(magicSet):
   table = magicSet('body table:last > tr')
-  releaseId = ''
-  totalHigh = 0
-  totalMedium = 0
-  totalLow = 0
 
   for row in table:
     d = pq(row);
     name = processHtml(d('td > font').html()).lower()
     release = magicSet('body table:first b').html().lower()
     cmc = processHtml(d('td:eq(1) > font').html()).lower()
-    cardType = processHtml(d('td:eq(2) > font').html()).lower()
-    color = processHtml(d('td:eq(3) > font').html()).lower()
+    cardType = fixCardType(processHtml(d('td:eq(2) > font').html()).lower())
+    color = fixColor(processHtml(d('td:eq(3) > font').html()).lower())
     rarity = processHtml(d('td:eq(4) > font').html()).lower()
     high = parseFloat(processHtml(d('td:eq(5) > font').html()))
     medium = parseFloat(processHtml(d('td:eq(6) > font').html()))
     low = parseFloat(processHtml(d('td:eq(7) > font').html()))
     
-    releaseId = getReleaseId(release)
-    totalHigh += high
-    totalMedium += medium
-    totalLow += low
-
     cardObj = {
       'name': name,
-      'release_id': releaseId,
+      'release': release,
       'cmc': cmc,
-      'type_id': getCardTypeId(cardType),
-      'color_id': getColorId(color),
-      'rarity_id': getRarityId(rarity),
+      'type': cardType,
+      'color': color,
+      'rarity': rarity,
     }
 
     cardId = getCardId(cardObj)
 
     value = {
-      'valuable_id': cardId,
-      'valuable_type': 'Card',
       'high': high,
       'medium': medium,
       'low': low,
       'date': datetime.datetime.utcnow(),
     }
 
-    valueId = values.insert(value)
-    cards.update( {"_id": cardId}, {"$push" : {"value_ids" : valueId}} )
-    
-  value = {
-    'valuable_id': releaseId,
-    'valuable_type': 'Release',
-    'high': totalHigh,
-    'medium': totalMedium,
-    'low': totalLow,
-    'date': datetime.datetime.utcnow(),
-  }
-  
-  valueId = values.insert(value)
-  releases.update( {"_id": cardId}, {"$push" : {"value_ids" : valueId}} )
-  print("Total : " + str(totalLow))
+    cards.update( {"_id": cardId}, {"$push" : {"values" : value}} )
 
 main = pq(url='http://magic.tcgplayer.com/magic_price_guides.asp')
 links = main('a.default_9')
