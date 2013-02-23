462f5fc6197e2c7cb9d087453ab79d0533d91dd5
David Hansen <david@tourian.(none)>
2013-02-23 11:24:51 -0700

    Switched to mysql

--- create.sql
+++ create.sql
@@ -1,36 +1,29 @@
 create table rarities( 
   id int not null auto_increment, 
   name varchar(100) not null, 
-  primary key (id) 
+  primary key (id),
+  unique(name) 
 );
 
 create table colors( 
   id int not null auto_increment, 
   name varchar(100) not null, 
-  primary key (id) 
+  primary key (id),
+  unique(name)
 );
 
 create table types( 
   id int not null auto_increment, 
   name varchar(100) not null, 
-  primary key (id) 
+  primary key (id),
+  unique(name)
 );
 
 create table releases( 
   id int not null auto_increment, 
   name varchar(100) not null, 
-  primary key (id) 
-);
-
-create table `values`( 
-  id int not null auto_increment, 
-  high DECIMAL(6,2) unsigned not null, 
-  medium DECIMAL(6,2) unsigned not null, 
-  low DECIMAL(6,2) unsigned not null, 
-  card int, 
-  created TIMESTAMP DEFAULT NOW(), 
-  primary key (id), 
-  foreign key (card) references cards(id)
+  primary key (id),
+  unique(name)
 );
 
 create table cards( 
@@ -45,5 +38,18 @@ create table cards(
   foreign key (`release`) references releases(id), 
   foreign key (type) references types(id), 
   foreign key (color) references colors(id), 
-  foreign key (rarity) references rarities(id) 
+  foreign key (rarity) references rarities(id),
+  unique(name, `release`)
 );
+
+create table `values`( 
+  id int not null auto_increment, 
+  high DECIMAL(6,2) unsigned not null, 
+  medium DECIMAL(6,2) unsigned not null, 
+  low DECIMAL(6,2) unsigned not null, 
+  card int, 
+  created TIMESTAMP DEFAULT NOW(), 
+  primary key (id), 
+  foreign key (card) references cards(id)
+);
+
--- parser.py
+++ parser.py
@@ -3,14 +3,11 @@
 from pyquery import PyQuery as pq
 from lxml import etree
 import urllib
-import pymongo
 import datetime
+import MySQLdb as mdb
+import sys
 
-from pymongo import Connection
-connection = Connection()
-landfall = connection.landfall
-#landfall.drop_collection('cards')
-cards = landfall.cards
+con = mdb.connect('localhost', 'root', 'acroman49', 'landfall')
 
 def getCardId(cardObj):
   card = cards.find_one(cardObj)
@@ -51,6 +48,12 @@ def fixColor(name):
   elif name == "gold":
     name = "multi"
   return name
+
+def fixValue(value):
+  if value == "":
+    return "0.00"
+  else:
+    return value
     
 def processHtml(html):
   if html is not None:
@@ -64,48 +67,52 @@ def parseFloat(text):
     return float(0)
 
 def parseSet(magicSet):
+  cur = con.cursor()
   table = magicSet('body table:last > tr')
 
   for row in table:
     d = pq(row);
-    name = processHtml(d('td > font').html()).lower()
-    release = magicSet('body table:first b').html().lower()
+    name = processHtml(d('td > font').html()).lower().replace("'", "\\'")
+    release = magicSet('body table:first b').html().lower().replace("'", "\\'")
     cmc = processHtml(d('td:eq(1) > font').html()).lower()
     cardType = fixCardType(processHtml(d('td:eq(2) > font').html()).lower())
     color = fixColor(processHtml(d('td:eq(3) > font').html()).lower())
     rarity = processHtml(d('td:eq(4) > font').html()).lower()
-    high = parseFloat(processHtml(d('td:eq(5) > font').html()))
-    medium = parseFloat(processHtml(d('td:eq(6) > font').html()))
-    low = parseFloat(processHtml(d('td:eq(7) > font').html()))
+    high = fixValue(processHtml(d('td:eq(5) > font').html()))
+    medium = fixValue(processHtml(d('td:eq(6) > font').html()))
+    low = fixValue(processHtml(d('td:eq(7) > font').html()))
+
+    print(name + " : " + low)
     
-    cardObj = {
-      'name': name,
-      'release': release,
-      'cmc': cmc,
-      'type': cardType,
-      'color': color,
-      'rarity': rarity,
-    }
+    cur.execute("insert ignore into rarities(name) values('" + rarity + "')")
+    cur.execute("insert ignore into types(name) values('" + cardType + "')")
+    cur.execute("insert ignore into colors(name) values('" + color + "')")
+    cur.execute("insert ignore into releases(name) values('" + release + "')")
 
-    cardId = getCardId(cardObj)
+    cur.execute("select id from rarities where name = '" + rarity + "'")
+    rarityId = str(cur.fetchone()[0])
+    cur.execute("select id from types where name = '" + cardType + "'")
+    typeId = str(cur.fetchone()[0])
+    cur.execute("select id from colors where name = '" + color + "'")
+    colorId = str(cur.fetchone()[0])
+    cur.execute("select id from releases where name = '" + release + "'")
+    releaseId = str(cur.fetchone()[0])
+    
+    cur.execute("insert ignore into cards(name, `release`, cmc, type, color, rarity) values('" + name + "', " + releaseId + ", '" + cmc + "', " + typeId + ", " + colorId + ", " + rarityId + ")")
+    
+    cur.execute("select id from cards where name = '" + name + "' and `release` = " + releaseId)
+    cardId = str(cur.fetchone()[0])
+    cur.execute("insert into `values`(high, medium, low, card) values('" + high + "', '" + medium + "', '" + low + "', " + cardId + ")")
 
-    value = {
-      'high': high,
-      'medium': medium,
-      'low': low,
-      'date': datetime.datetime.utcnow(),
-    }
+with con:
+  main = pq(url='http://magic.tcgplayer.com/magic_price_guides.asp')
+  links = main('a.default_9')
+  sets = []
 
-    cards.update( {"_id": cardId}, {"$push" : {"values" : value}} )
+  for link in links:
+    sets.append(pq(link).attr('href'))
 
-main = pq(url='http://magic.tcgplayer.com/magic_price_guides.asp')
-links = main('a.default_9')
-sets = []
-
-for link in links:
-  sets.append(pq(link).attr('href'))
-
-for set in sets:
-  address = 'http://magic.tcgplayer.com' + set.replace(' ', '%20')
-  print("parsing: " + address)
-  parseSet(pq(url=address))
+  for set in sets:
+    address = 'http://magic.tcgplayer.com' + set.replace(' ', '%20')
+    print("parsing: " + address)
+    parseSet(pq(url=address))
