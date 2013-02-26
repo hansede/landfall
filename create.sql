e09b84f6cf9e32f17672be957db342eb5841c6d6
David Hansen <david@tourian.(none)>
2013-02-25 23:59:15 -0700

    Fixes for mysql

--- create.sql
+++ create.sql
@@ -1,55 +1,23 @@
-create table rarities( 
-  id int not null auto_increment, 
-  name varchar(100) not null, 
-  primary key (id),
-  unique(name) 
-);
-
-create table colors( 
-  id int not null auto_increment, 
-  name varchar(100) not null, 
-  primary key (id),
-  unique(name)
-);
-
-create table types( 
-  id int not null auto_increment, 
-  name varchar(100) not null, 
-  primary key (id),
-  unique(name)
-);
-
-create table releases( 
-  id int not null auto_increment, 
-  name varchar(100) not null, 
-  primary key (id),
-  unique(name)
-);
-
 create table cards( 
   id int not null auto_increment, 
   name varchar(100) not null, 
-  `release` int, 
+  rel varchar(100) not null,
   cmc varchar(100) not null, 
-  type int, 
-  color int, 
-  rarity int, 
+  type varchar(100) not null,
+  color varchar(20) not null,
+  rarity varchar(2) not null,
   primary key (id), 
-  foreign key (`release`) references releases(id), 
-  foreign key (type) references types(id), 
-  foreign key (color) references colors(id), 
-  foreign key (rarity) references rarities(id),
-  unique(name, `release`)
+  unique(name, rel)
 );
 
-create table `values`( 
+create table prices( 
   id int not null auto_increment, 
   high DECIMAL(6,2) unsigned not null, 
   medium DECIMAL(6,2) unsigned not null, 
   low DECIMAL(6,2) unsigned not null, 
-  card int, 
+  card_id int, 
   created TIMESTAMP DEFAULT NOW(), 
   primary key (id), 
-  foreign key (card) references cards(id)
+  foreign key (card_id) references cards(id)
 );
 
--- parser.py
+++ parser.py
@@ -54,6 +54,12 @@ def fixValue(value):
     return "0.00"
   else:
     return value
+
+def fixRelease(name):
+  if "urza''s" in name:
+    return name.replace("urza''s", "urza's")
+  else:
+    return name
     
 def processHtml(html):
   if html is not None:
@@ -73,7 +79,7 @@ def parseSet(magicSet):
   for row in table:
     d = pq(row);
     name = processHtml(d('td > font').html()).lower().replace("'", "\\'")
-    release = magicSet('body table:first b').html().lower().replace("'", "\\'")
+    release = fixRelease(magicSet('body table:first b').html().lower()).replace("'", "\\'")
     cmc = processHtml(d('td:eq(1) > font').html()).lower()
     cardType = fixCardType(processHtml(d('td:eq(2) > font').html()).lower())
     color = fixColor(processHtml(d('td:eq(3) > font').html()).lower())
@@ -82,27 +88,11 @@ def parseSet(magicSet):
     medium = fixValue(processHtml(d('td:eq(6) > font').html()))
     low = fixValue(processHtml(d('td:eq(7) > font').html()))
 
-    print(name + " : " + low)
+    cur.execute("insert ignore into cards(name, rel, cmc, type, color, rarity) values('" + name + "', '" + release + "', '" + cmc + "', '" + cardType + "', '" + color + "', '" + rarity + "')")
     
-    cur.execute("insert ignore into rarities(name) values('" + rarity + "')")
-    cur.execute("insert ignore into types(name) values('" + cardType + "')")
-    cur.execute("insert ignore into colors(name) values('" + color + "')")
-    cur.execute("insert ignore into releases(name) values('" + release + "')")
-
-    cur.execute("select id from rarities where name = '" + rarity + "'")
-    rarityId = str(cur.fetchone()[0])
-    cur.execute("select id from types where name = '" + cardType + "'")
-    typeId = str(cur.fetchone()[0])
-    cur.execute("select id from colors where name = '" + color + "'")
-    colorId = str(cur.fetchone()[0])
-    cur.execute("select id from releases where name = '" + release + "'")
-    releaseId = str(cur.fetchone()[0])
-    
-    cur.execute("insert ignore into cards(name, `release`, cmc, type, color, rarity) values('" + name + "', " + releaseId + ", '" + cmc + "', " + typeId + ", " + colorId + ", " + rarityId + ")")
-    
-    cur.execute("select id from cards where name = '" + name + "' and `release` = " + releaseId)
+    cur.execute("select id from cards where name = '" + name + "' and rel = '" + release + "'")
     cardId = str(cur.fetchone()[0])
-    cur.execute("insert into `values`(high, medium, low, card) values('" + high + "', '" + medium + "', '" + low + "', " + cardId + ")")
+    cur.execute("insert into prices(high, medium, low, card_id) values('" + high + "', '" + medium + "', '" + low + "', " + cardId + ")")
 
 with con:
   main = pq(url='http://magic.tcgplayer.com/magic_price_guides.asp')
