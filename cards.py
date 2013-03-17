75c260c3f16897f60cffbe04bf17335f7d318fc8
David Hansen <david@tourian.(none)>
2013-03-17 12:57:57 -0600

    Changes to card image parser and a couple of queries.

--- cards.py
+++ cards.py
@@ -4,10 +4,17 @@ from pyquery import PyQuery as pq
 from lxml import etree
 import urllib2
 
-count = 1
+count = 137045
 
-while True:
-  name = pq("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=" + str(count))('.contentTitle > span').text().lower()
+while count <= 1000000:
+  print(count)
+  text = pq("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=" + str(count))('.contentTitle > span').text()
+  
+  if text is None:
+    count += 1
+    continue
+    
+  name = text.lower()
   image = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + str(count) + "&type=card"
   print(name)
   print(image)
@@ -16,3 +23,9 @@ while True:
   localFile.write(u.read())
   localFile.close()
   count += 1
+  
+#3756
+#3757
+#4312
+#4669
+#4694
